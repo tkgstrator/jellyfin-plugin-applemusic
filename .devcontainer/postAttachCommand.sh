63cb68332180048078aaf6b255e2e4cc0d5f0406
tkgstrator <tkgstrator@users.noreply.github.com>
2025-11-23 21:22:37 +0000

    chore: add dev container configuration with Docker support

---
+++ .devcontainer/postAttachCommand.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+git config --global --unset commit.template
+git config --global --add safe.directory /home/vscode/app
+git config --global fetch.prune true
+git config --global --add --bool push.autoSetupRemote true
