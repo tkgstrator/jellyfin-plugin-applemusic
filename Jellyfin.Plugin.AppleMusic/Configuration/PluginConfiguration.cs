859f9d436b184ed1595a009999c0313d1762490f
tkgstrator <nasawake.am@gmail.com>
2025-11-24 16:01:03 +0000

    refactor(plugin): simplify configuration and remove unused region settings

--- Jellyfin.Plugin.AppleMusic/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.AppleMusic/Configuration/PluginConfiguration.cs
@@ -4,39 +4,9 @@ using MediaBrowser.Model.Plugins;
 namespace Jellyfin.Plugin.AppleMusic.Configuration
 {
     /// <summary>
-    /// Apple Music region types.
-    /// </summary>
-    public enum RegionType
-    {
-        /// <summary>
-        /// United States.
-        /// </summary>
-        [Description("us")]
-        US,
-
-        /// <summary>
-        /// Japan.
-        /// </summary>
-        [Description("jp")]
-        JA
-    }
-
-    /// <summary>
     /// The plugin configuration.
     /// </summary>
     public class PluginConfiguration : BasePluginConfiguration
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
-        /// </summary>
-        public PluginConfiguration()
-        {
-            Region = RegionType.JA;
-        }
-
-        /// <summary>
-        /// Gets or sets the Apple Music region.
-        /// </summary>
-        public RegionType Region { get; set; } = RegionType.JA;
     }
 }
